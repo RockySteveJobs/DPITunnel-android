ac4e34b2be80db93fac8732e3ad6a55e6310eef8
zhenyolka <zhenyolka@gmail.com>
2022-03-05 19:53:49 +0300

    Add preconfigured profile to DB

--- app/src/main/java/ru/evgeniy/dpitunnelcli/database/AppDatabase.kt
+++ app/src/main/java/ru/evgeniy/dpitunnelcli/database/AppDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [Profile::class], version = 1)
 @TypeConverters(Converters::class)
@@ -23,13 +27,44 @@ abstract class AppDatabase : RoomDatabase() {
                         context.applicationContext,
                         AppDatabase::class.java,
                         "app_database"
-                    ).fallbackToDestructiveMigration().build()
+                    )
+                        .addCallback(object : Callback() {
+                            override fun onCreate(db: SupportSQLiteDatabase) {
+                                prepopulateDb(getInstance(context))
+                            }
+                        })
+                        .fallbackToDestructiveMigration().build()
 
                     INSTANCE = instance
                 }
 
                 return instance
             }
+        }
+
+        private fun prepopulateDb(db: AppDatabase) = CoroutineScope(Dispatchers.Default).launch {
+            db.profileDao().insertProfile(
+                Profile(
+                    id = null,
+                    name = "default",
+                    title = "Auto TTL universal",
+                    bufferSize = null,
+                    splitPosition = 3,
+                    splitAtSni = true,
+                    autoTtl = true,
+                    fakePacketsTtl = null,
+                    windowSize = 1,
+                    windowScaleFactor = 6,
+                    inBuiltDNS = true,
+                    inBuiltDNSIP = "8.8.8.8",
+                    inBuiltDNSPort = null,
+                    doh = true,
+                    dohServer = "https://dns.google/dns-query",
+                    desyncAttacks = true,
+                    desyncZeroAttack = DesyncZeroAttack.DESYNC_ZERO_FAKE,
+                    desyncFirstAttack = DesyncFirstAttack.DESYNC_FIRST_DISORDER_FAKE
+            )
+            )
         }
     }
 }
